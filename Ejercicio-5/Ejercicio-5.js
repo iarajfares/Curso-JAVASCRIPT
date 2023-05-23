366d15012cf32e7242b5a945eaf5a58bd52118e0
Iara <131480302+iarajfares@users.noreply.github.com>
2023-05-23 19:04:30 -0300

    Add files via upload

---
+++ Ejercicio-5/Ejercicio-5.js
@@ -0,0 +1,8 @@
+let altura = 155;
+let altura_2 = 1.55;
+let peso = 50.5;
+
+console.log(Math.ceil(altura_2));
+console.log(Math.floor(peso));
+
+console.log(Number.MAX_VALUE + 1 === Number.MAX_VALUE);
