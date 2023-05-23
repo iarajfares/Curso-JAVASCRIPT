2cded32b62b6d87aad749dbf28c3b014b6e3d62b
Iara <131480302+iarajfares@users.noreply.github.com>
2023-05-23 15:55:01 -0300

    Add files via upload

---
+++ Ejercicio-4/Ejercicio-4.js
@@ -0,0 +1,14 @@
+let str_1 = "Iara";
+let str_2 = "Fares";
+let estudiante = str_1.concat(" ", str_2);
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+console.log(estudiante)
+console.log(estudianteMayus)
+console.log(estudianteMinus)
+console.log(estudiante.length)
+console.log(estudiante.indexOf("I"))
+console.log(estudiante.lastIndexOf("s"))
+console.log(estudiante.trim().length)
+console.log(estudiante.includes("Iara Fares"))
